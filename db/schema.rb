20a104ea43a70d1a3d4b9e8c81868d90b980f99e
Ashish Darji <ashdarji1@gmail.com>
2021-07-25 18:37:53 -0400

    styling the signup and sign in forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_212457) do
+ActiveRecord::Schema.define(version: 2021_07_25_220638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_07_25_212457) do
     t.string "last_name"
     t.string "role"
     t.boolean "is_admin"
+    t.boolean "terms_of_service"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
