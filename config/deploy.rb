40540af385b6fb7323ed394a59196cb46996691a
Ashish Darji <ashdarji1@gmail.com>
2021-08-04 22:26:28 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
+set :passenger_restart_with_touch, true
 
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
